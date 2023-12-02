58d181584e2cc360f3c8afd0aec1d1f40df61741
Stephan Reichholf <stephan@reichholf.net>
2023-12-02 11:32:30 +0100

    Add copyright, remove temporary comment

--- keyboards/redragon/k552/v2/iso/keymaps/via/keymap.c
+++ keyboards/redragon/k552/v2/iso/keymaps/via/keymap.c
@@ -1,5 +1,6 @@
 /*
 Copyright 2019 Stephen Peery
+Copyright 2023 Stephan Reichholf
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
